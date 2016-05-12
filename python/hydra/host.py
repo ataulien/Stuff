ad4a0caa4b0b12014766ded7d92f2ea5e242c531
Andre <andre@desktop.fritz.box>
2016-05-12 21:24:30 +0200

    ---

--- python/hydra/host.py
+++ python/hydra/host.py
@@ -1,25 +1,25 @@
 import subprocess
 
 def getNumactlHardware():
-    #return subprocess.check_output(["numactl", "--hardware"])
-    f = open("numactl_hardware.txt", "r")
+    return subprocess.check_output(["numactl", "--hardware"])
+    # f = open("numactl_hardware.txt", "r")
 
-    return f.read();
+    # return f.read();
     
 def getDplaceQQQ():
-    #return subprocess.check_output(["dplace", "-qqq"])
-    f = open("place_qqq.txt", "r")
+    return subprocess.check_output(["dplace", "-qqq"])
+    # f = open("place_qqq.txt", "r")
 
-    return f.read();
+    # return f.read();
     
 def getDlookByName(name):
-    # out = subprocess.check_output(["dlook", name])
+    out = subprocess.check_output(["dlook", name])
   
-    f = open("dlook.txt", "r")
-    out = f.read();
+    # f = open("dlook.txt", "r")
+    # out = f.read();
     
-    if not out.startswith("Peek:"):
-        print("dlook failed. '" + out + "'")
-        return ""
+    # if not out.startswith("Peek:"):
+    #     print("dlook failed. '" + out + "'")
+    #     return ""
         
-    return out
+    # return out
