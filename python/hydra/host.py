4deafc1b17b59104e917a267c69f5111d1f5f59f
Andre <andre@desktop.fritz.box>
2016-05-12 22:38:32 +0200

    ---

--- python/hydra/host.py
+++ python/hydra/host.py
@@ -4,7 +4,7 @@ if "check_output" not in dir( subprocess ): # duck punch it in!
     def f(*popenargs, **kwargs):
         if 'stdout' in kwargs:
             raise ValueError('stdout argument not allowed, it will be overridden.')
-        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+        process = subprocess.Popen(stdout=subprocess.PIPE, stderr=subprocess.PIPE, *popenargs, **kwargs) # Thanks dlook, for just writing everything to stderr!
         output, unused_err = process.communicate()
         retcode = process.poll()
         if retcode:
@@ -28,21 +28,8 @@ def getDplaceQQQ():
 
     # return f.read();
     
-def getDlookByName(name):
-    
-    if not name.isdigit():
-        pids_str = subprocess.check_output(["pgrep", name])
-        out = subprocess.check_output(["dlook", pid])
-    else:
-        out = subprocess.check_output(["dlook", name])
-        
-    # f = open("dlook.txt", "r")
-    # out = f.read();
-    
-    # if not out.startswith("Peek:"):
-    #     print("dlook failed. '" + out + "'")
-    #     return ""
-        
+def getDlookByPID(pid):
+    out = subprocess.check_output(["dlook", pid])        
     return out
     
 def getPIDListByProcName(name):
