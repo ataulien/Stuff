32f6f1a90505331fb3b31cd91b5a569a69369012
Andre <andre@desktop.fritz.box>
2016-05-12 21:26:50 +0200

    ---

--- python/hydra/host.py
+++ python/hydra/host.py
@@ -1,5 +1,20 @@
 import subprocess
 
+if "check_output" not in dir( subprocess ): # duck punch it in!
+    def f(*popenargs, **kwargs):
+        if 'stdout' in kwargs:
+            raise ValueError('stdout argument not allowed, it will be overridden.')
+        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+        output, unused_err = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            cmd = kwargs.get("args")
+            if cmd is None:
+                cmd = popenargs[0]
+            raise subprocess.CalledProcessError(retcode, cmd)
+        return output
+    subprocess.check_output = f
+
 def getNumactlHardware():
     return subprocess.check_output(["numactl", "--hardware"])
     # f = open("numactl_hardware.txt", "r")
